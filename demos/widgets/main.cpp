567a75c1d0d0306e8b28b750556de77646d9d0f9
Dalphat <44422914+Dalphat@users.noreply.github.com>
2019-01-06 00:14:29 -0800

    RAII for main stub

--- demos/widgets/main.cpp
+++ demos/widgets/main.cpp
@@ -4,39 +4,56 @@
 #include <widgets/Text.hpp>
 #include <widgets/Button.hpp>
 #include <widgets/Scale.hpp>
+class Demos{
+private:
+    Blame::Console          *console;
+    Blame::Widgets::Window  *window;
+    Blame::Widgets::Text    *text,
+                            *scale_text;
+    Blame::Widgets::Button  *button;
+    Blame::Widgets::Scale   *scale;
+public:
+    Demos(){
+        console = new Blame::Console();
+        console->setTitle("Hello, World!");
 
+        window = new Blame::Widgets::Window(console, "Hello World");
+        window->place(5, 5, 22, 18);
+
+        text = new Blame::Widgets::Text(console, window);
+        text->pack(Blame::Util::Direction::DOWN);
+
+        button = new Blame::Widgets::Button(console, window, "Click Me", nullptr);
+        button->command = [=]() {button->text = "Clicked!";};
+        button->pack(Blame::Util::Direction::DOWN);
+
+        scale = new Blame::Widgets::Scale(console, window, Blame::Util::Orientation::HORIZONTAL, nullptr);
+        scale->pack(Blame::Util::Direction::DOWN);
+        scale->min = -1;
+        scale->max = 1;
+
+        scale_text = new Blame::Widgets::Text(console, window);
+        scale_text->pack(Blame::Util::Direction::DOWN);
+        scale_text->width = 8;
+        scale_text->height = 3;
+        scale_text->state = Blame::Util::State::DISABLED;
+
+        scale->command = [=](){
+            scale_text->content.push_back(std::to_string(scale->current));
+        };
+
+        console->mainLoop();
+    }
+    ~Demos(){
+        delete console;
+        delete window;
+        delete text;
+        delete button;
+        delete scale;
+        delete scale_text;
+    }
+};
 int main() {
-    auto console = new Blame::Console();
-    console->setTitle("Hello, World!");
-    // console->setTitle(std::to_string(console->width) + "x" + std::to_string(console->height) + "=" + std::to_string(console->width * console->height));
-
-    auto window = new Blame::Widgets::Window(console, "Hello World");
-    window->place(5, 5, 22, 18);
-
-    auto text = new Blame::Widgets::Text(console, window);
-    text->pack(Blame::Util::Direction::DOWN);
-
-    auto button = new Blame::Widgets::Button(console, window, "Click Me", nullptr);
-    button->command = [=]() {button->text = "Clicked!";};
-    button->pack(Blame::Util::Direction::DOWN);
-
-    auto scale = new Blame::Widgets::Scale(console, window, Blame::Util::Orientation::HORIZONTAL, nullptr);
-    scale->pack(Blame::Util::Direction::DOWN);
-    scale->min = -1;
-    scale->max = 1;
-
-    auto scale_text = new Blame::Widgets::Text(console, window);
-    scale_text->pack(Blame::Util::Direction::DOWN);
-    scale_text->width = 8;
-    scale_text->height = 3;
-    scale_text->state = Blame::Util::State::DISABLED;
-
-    scale->command = [=]() {scale_text->content[0] = std::to_string(scale->current);};
-
-    // auto window_two = new Blame::Widgets::Window(console, "Another Window");
-    // window_two->place(40, 8, 30, 10);
-
-    console->mainLoop();
-
+    Demos demos;
     return 0;
 }
