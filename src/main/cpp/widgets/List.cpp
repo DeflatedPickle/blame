fe159f9564e7824f55f438e10f42378c367b19c7
Vince <allLogarithmsEqual@protonmail.com>
2021-04-16 18:28:04 -0400

    Revert "Add simple debug output to diagnose segfault case"
    This reverts commit 55fd880967b46171d277f934a55d1dbd7f45d036.

--- src/main/cpp/widgets/List.cpp
+++ src/main/cpp/widgets/List.cpp
@@ -20,35 +20,16 @@ void Blame::Widgets::List::redraw() {
     Widget::redraw();
 
     int iteration = 0;
-    for (auto item = 0; item < this->content.size(); item++) {
-        for (auto i = 0; i < this->content[item].length(); i++) {
-            
-            if (this->client_area.top + iteration < this->view_area.bottom - 2 &&
-                this->client_area.left + i < this->view_area.right) {
-                
-                if ((item + this->view_area_offset_y) >= this->content.size()) {
-                    std::cout << "[ ERR ]    This is the case where our current item index, ";
-                    std::cout << " " << item << ", plus the view_area_offset_y, ";
-                    std::cout << " " << view_area_offset_y << ", is far too high!! :o";
-                    std::cout << std::endl;
-
-                    std::cout << "[     ]    Notice that the only reason we\'re TRYING a redraw";
-                    std::cout << " is because the y-axis iteration is " << iteration << std::endl;
-                    std::cout << "[     ]    which is less than the \"bottom minus two\" i.e. " << (this->view_area.bottom - 2) << std::endl;
-                    this->console->raw_grid[this->client_area.top + iteration][this->client_area.left + i] =
-                            this->getCurrentColour(this->style.colours.background_content)
-                            + (iteration == this->selection ? Blame::Util::EscapeCodes::backgroundBlue()
-                                                            + Blame::Util::EscapeCodes::foregroundYellow()
-                                                          : this->getCurrentColour(this->style.colours.text))
-                            + this->content[item + this->view_area_offset_y][i + this->view_area_offset_x];
-                } else {
-                    this->console->raw_grid[this->client_area.top + iteration][this->client_area.left + i] =
-                            this->getCurrentColour(this->style.colours.background_content)
-                            + (iteration == this->selection ? Blame::Util::EscapeCodes::backgroundBlue()
-                                                            + Blame::Util::EscapeCodes::foregroundYellow()
-                                                          : this->getCurrentColour(this->style.colours.text))
-                            + this->content[item + this->view_area_offset_y][i + this->view_area_offset_x];
-                }
+    for (auto item = 0; item < this->items.size(); item++) {
+        for (auto i = 0; i < this->items[item].length(); i++) {
+            if (this->client_area.top + iteration < this->view_area.bottom - 2
+                && this->client_area.left + i < this->view_area.right) {
+                this->console->raw_grid[this->client_area.top + iteration][this->client_area.left + i] =
+                        this->getCurrentColour(this->style.colours.background_content)
+                        + (iteration == this->selection ? Blame::Util::EscapeCodes::backgroundBlue()
+                                                        + Blame::Util::EscapeCodes::foregroundYellow()
+                                                      : this->getCurrentColour(this->style.colours.text))
+                        + this->items[item + this->view_area_offset_y][i + this->view_area_offset_x];
             }
         }
 
@@ -57,4 +38,3 @@ void Blame::Widgets::List::redraw() {
 
     this->is_redrawn.exchange(true);
 }
-
