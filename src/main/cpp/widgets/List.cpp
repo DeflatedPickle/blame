0111849c052774ad70d5893f3aaf8af29bac2e2b
Vince <allLogarithmsEqual@protonmail.com>
2021-04-16 18:31:01 -0400

    Fix references to the Scrollable items (content)

--- src/main/cpp/widgets/List.cpp
+++ src/main/cpp/widgets/List.cpp
@@ -20,8 +20,8 @@ void Blame::Widgets::List::redraw() {
     Widget::redraw();
 
     int iteration = 0;
-    for (auto item = 0; item < this->items.size(); item++) {
-        for (auto i = 0; i < this->items[item].length(); i++) {
+    for (auto item = 0; item < this->content.size(); item++) {
+        for (auto i = 0; i < this->content[item].length(); i++) {
             if (this->client_area.top + iteration < this->view_area.bottom - 2
                 && this->client_area.left + i < this->view_area.right) {
                 this->console->raw_grid[this->client_area.top + iteration][this->client_area.left + i] =
@@ -29,7 +29,7 @@ void Blame::Widgets::List::redraw() {
                         + (iteration == this->selection ? Blame::Util::EscapeCodes::backgroundBlue()
                                                         + Blame::Util::EscapeCodes::foregroundYellow()
                                                       : this->getCurrentColour(this->style.colours.text))
-                        + this->items[item + this->view_area_offset_y][i + this->view_area_offset_x];
+                        + this->content[item + this->view_area_offset_y][i + this->view_area_offset_x];
             }
         }
 
